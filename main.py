1b8d634f3baba0c16ebc79c05e11980da65be187
Kyliam <nguyencaoky3092003@gmail.com>
2026-06-03 13:03:01 +0700

    message and fix bugs

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("This is new branch!", "It's theory branch", sep=" ", end="\n")
+butterflies = 10
+beetles = 12
+ladybugs = 20
+
+print('I caught ' + str(butterflies) + ' 🦋 butterflies!')
+print('I caught ' + str(beetles) + ' 🪲 beetles!')
+print('I caught ' + str(ladybugs) + ' 🐞 ladybugs!')
+
+total = butterflies + beetles + ladybugs
+print('I caught ' + str(total) + ' total bugs!')
